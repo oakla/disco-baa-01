a51585166d297db79204e573fa7f65dce7936d46
Alexander Oakley <2579439+oakla@users.noreply.github.com>
2026-01-15 00:57:16 +0800

    polish path defs

--- src/definitions.py
+++ src/definitions.py
@@ -1,5 +1,16 @@
 from pathlib import Path
 
+DEMO_MODE = False
+
+
+
+# NOTE: Some files overwritten if DEMO_MODE is True
 ROOT_DIR = Path(__file__).parent.parent
 DATA_DIR = ROOT_DIR / "data"
-RAW_DATA_DIR = DATA_DIR / "raw"
+INCOMING_DATA_DIR = DATA_DIR / "00_incoming"
+RAW_DATA_DIR = DATA_DIR / "01_raw"
+
+INTERIM_DATA_DIR = DATA_DIR / "02_interim"
+
+if DEMO_MODE:
+    RAW_DATA_DIR = DATA_DIR / "01_raw_sample"
